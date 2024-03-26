cbc3f0c2d11a77b607068b1bd634871d191a4b63
Adarsh Raj <89922781+silentkiller18@users.noreply.github.com>
2024-03-26 19:27:20 +0530

    Time: 3 ms (99.84%) | Memory: 12.8 MB (47.23%) - LeetSync

--- 126-word-ladder-ii/word-ladder-ii.cpp
+++ 126-word-ladder-ii/word-ladder-ii.cpp
@@ -1,76 +1,76 @@
 class Solution {
-    unordered_map<string, int>map;
-    vector<vector<string>> ans;
-    string b;
-    
-    void dfs(string word, vector<string> &seq)
-    {
-        if(word==b)
-        {
-            reverse(seq.begin(), seq.end());
-            ans.push_back(seq);
-            reverse(seq.begin(), seq.end());
+public:
+    // TC -- Can not be determined it depends on example to example.
+    // Below is the striver explanation.
+    void dfs(string word , vector<string> &sq , vector<vector<string>> &ans , string beginWord , int n , unordered_map<string , int> &mp){
+        // Base case
+        if(word == beginWord){
+            reverse(sq.begin() , sq.end());
+            ans.push_back(sq);
+            reverse(sq.begin() , sq.end());
             return;
         }
-        
-        int steps=map[word];
-        int size=word.size();
-        for(int i=0; i<size; i++)
-            {
-                char original=word[i];
-                for(char c='a'; c<='z'; c++)
-                {
-                    word[i]=c;
-                    if(map.find(word)!=map.end() && map[word]+1==steps)
-                    {
-                        seq.push_back(word);
-                        dfs(word, seq);
-                        seq.pop_back();
-                    }
+
+        int steps = mp[word];
+
+        for(int i = 0; i < n; i++){
+            char original = word[i];
+            for(char ch = 'a'; ch <= 'z'; ch++){
+                word[i] = ch;
+                // If this transfomed word is present in the map also it should be in the different level then only we will add it in the sequence.
+                if(mp.find(word) != mp.end() && mp[word] + 1 == steps){
+                    sq.push_back(word);
+                    dfs(word , sq , ans , beginWord , n , mp);
+                    sq.pop_back();
                 }
-                word[i]=original;
             }
+            word[i] = original;
+        }
         
     }
-    
-public:
+
     vector<vector<string>> findLadders(string beginWord, string endWord, vector<string>& wordList) {
-        b=beginWord;
-        unordered_set<string>set(wordList.begin(), wordList.end());
-        queue<string>q;
-        q.push({beginWord});
-        map[beginWord]=1;
-        int size=beginWord.size();
-        set.erase(beginWord);
-        
-        while(!q.empty())
-        {
-            string word=q.front();
-            int steps=map[word];
+        vector<vector<string>> ans;
+        int n = beginWord.size();
+        unordered_set<string> st(wordList.begin() , wordList.end());
+        unordered_map<string , int> mp;
+        mp[beginWord] = 1;
+        queue<string> q;
+        q.push(beginWord);
+        st.erase(beginWord);
+
+        while(!q.empty()){
+            string word = q.front();
+            int steps = mp[word];
             q.pop();
-            if(word==endWord) break;
-            for(int i=0; i<size; i++)
-            {
-                char original=word[i];
-                for(char c='a'; c<='z'; c++)
-                {
-                    word[i]=c;
-                    if(set.count(word))
-                    {
-                        q.push({word});
-                        set.erase(word);
-                        map[word]=steps+1;
+
+            // Means our map is ready jo begin word se end work tak ke transformations strings ko hold karke betha hai with their sequence length , Now its time to backtrack.
+            if(word == endWord){
+                break;
+            }
+
+            for(int i = 0; i < n; i++){
+                char original = word[i];
+                for(char ch = 'a'; ch <= 'z'; ch++){
+                    word[i] = ch;
+                    // Means yeh transfomed word hamare wordlist mai present hai .
+                    if(st.find(word) != st.end()){
+                        q.push(word);
+                        mp[word] = steps + 1;
+                        st.erase(word);
                     }
                 }
-                word[i]=original;
+                word[i] = original;
             }
         }
-        if(map.find(endWord)!=map.end())
-        {
-            vector<string> seq;
-            seq.push_back(endWord);
-            dfs(endWord, seq);
+
+        // Agar end word part hai map ka it means begin se end jaana possible hai to we will backtrack otherwise hum null return kardenge.
+        if(mp.find(endWord) != mp.end()){
+            vector<string> sq;
+            sq.push_back(endWord);
+            dfs(endWord , sq , ans , beginWord , n , mp);
         }
+
         return ans;
     }
 };
