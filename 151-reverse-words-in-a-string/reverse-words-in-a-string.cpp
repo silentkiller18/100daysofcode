9dfa9d7c8751ab3938d0c0c6b62dbc88acf970f1
Adarsh Raj <89922781+silentkiller18@users.noreply.github.com>
2024-07-14 15:29:14 +0530

    Time: 8 ms (23.12%) | Memory: 10.1 MB (46.75%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -1,27 +1,26 @@
-#include<bits/stdc++.h>
 class Solution {
 public:
-    string reverseWords(string str) {
-        stringstream s(str);
-        string word;
-
-        // to store words.
-        vector<string>temp;
-        // to store the ans.
-        string ans="";
-
-        while(s>>word){
-            temp.push_back(word);
+    string reverseWords(string s) {
+        stack<string> st;
+        string a;
+        for(int i=0;i<s.size();i++){
+            if(s[i]!= ' '){
+                a.push_back(s[i]);
+            } else if(!a.empty()){
+                st.push(a);
+                a.clear();
+            }
         }
-        //now store the words in reverse order and add the extra space at the end of eaach word. except the first one.
-
-        for(int i=temp.size()-1;i>=0;i--){
-            if(i!=0)
-            ans+=temp[i]+" ";
-            else
-                ans+=temp[i];
-
+        if(!a.empty()) st.push(a);
+        string ans;
+        while(!st.empty()){
+            // if(st.top()=="") continue;
+            // else {
+                ans += st.top() + " ";
+                st.pop();
+            // }
         }
+        ans.pop_back(); // remove the extra space at the end
         return ans;
     }
 };
