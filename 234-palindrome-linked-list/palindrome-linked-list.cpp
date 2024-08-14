b1648cd9a2d1afe1b574ae17be785d9aa631f4b6
Adarsh Raj <89922781+silentkiller18@users.noreply.github.com>
2024-08-14 23:13:59 +0530

    Time: 184 ms (30.01%) | Memory: 131.1 MB (8.09%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -8,38 +8,20 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
- #include<bits/stdc++.h>
 class Solution {
 public:
     bool isPalindrome(ListNode* head) {
- ListNode* fast = head;
-        ListNode* slow = head;
-        
-        while(fast && fast->next){
-            fast = fast->next->next;
-            slow = slow->next;
-        }
-        
-        if(fast) slow = slow->next;
-        slow = reverse(slow);
-        
-        while(slow){
-            if(slow->val != head->val) return false;
-            slow = slow->next;
-            head = head->next;
-        }
-        return true;
-    }
-private:
-    ListNode* reverse(ListNode* head){
-        ListNode* prev = NULL;
-        ListNode* curr= head;
-        while(curr){
-            curr= curr->next;
-            head->next = prev;
-            prev = head;
-            head = curr;
-        }
-        return prev;
+       vector<int>list;
+       while(head){
+        list.push_back(head->val);
+        head=head->next;
+       } 
+
+       int left=0,right=list.size()-1;
+       while(left<right&&list[left]==list[right]){
+        left++;
+        right--;
+       }
+       return left>=right;
     }
 };
